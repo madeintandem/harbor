35216e0cef3a1526442b74824a3787b4548c2ae6
Erin Hochstatter <ehochstatter@gmail.com>
2014-08-07 12:24:18 -0500

    Initial Commit

---
+++ Harbor/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Harbor
+//
+//  Created by Erin Hochstatter on 8/7/14.
+//  Copyright (c) 2014 devMynd. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
