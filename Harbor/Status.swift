d6b765358cd62e67522231fca2be6a6cba464577
Michael Crismali <michael.crismali@gmail.com>
2016-05-11 13:23:38 -0500

    Pulled out Status enum

---
+++ Harbor/Status.swift
@@ -0,0 +1,11 @@
+//
+//  Status.swift
+//  Harbor
+//
+//  Created by Michael Crismali on 5/11/16.
+//  Copyright © 2016 DevMynd. All rights reserved.
+//
+
+enum Status {
+    case Success, Error, Building
+}
